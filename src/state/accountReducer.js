a43b81c40a1b4f56aebbd5d4ff6909e2f517651c
HM1 <mrtoscano94@gmail.com>
2022-03-09 09:20:13 -0500

    First Redux Crash course

---
+++ src/state/accountReducer.js
@@ -0,0 +1,23 @@
+// ? A reducer is a function that return state
+// ? 1.
+// ? We can see the reducer as the banker
+// ? 2.
+// ? He receivess the envelope with an action type and payload
+// ? 3.
+// ? Depending on the action type the banker decides what todo with the payload
+// ? 4.
+// ? In the case of the deposit, is the amount the king watns to deposit
+
+const bankerReducer = (state = 0, action) => {
+  switch (action) {
+    case "Increase":
+      return state + action.payload;
+    case "Decrease":
+      return state - action.payload;
+    default:
+      return state
+  }
+};
+
+
+export default bankerReducer
